90acb917fa56304c88fdeb925246a62a05ce3841
jack <543612779@qq.com>
2017-12-07 11:27:56 +0800

    Revert "更新antd版本"
    This reverts commit 0c071cf288044c3e815b37a9fc47e799c1bdb44d.

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ import Redbox from 'redbox-react';
 // AppContainer 是一个 HMR 必须的包裹(wrapper)组件
 import RouterConfig from './Router'; // 路由配置
 import store from './Store'; // 引入Store
-import { LocaleProvider } from 'antd';
-import zhCN from 'antd/lib/locale-provider/zh_CN';
+
 const history = createHistory();
 
 // 订阅state改变
