3b7ce43632e9c30e29a8816dce97d436a012ac24
jack <543612779@qq.com>
2018-01-25 18:22:27 +0800

    test router

--- test/Fullrender/Home.FullRender.test.js
+++ test/Fullrender/Home.FullRender.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { expect } from 'chai';
+import { Router } from 'react-router-dom';
 import ReactTestUtils from 'react-dom/test-utils';
 import { shallow, render, mount } from 'enzyme';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { Router } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
-import Home from '../../src/Containers/Home';
+import Entry from '../../src/Containers/Entry';
 import Reducer from '../../src/Reducer';
 import sinon from 'sinon';
 
@@ -14,30 +14,33 @@ const history = createHistory();
 // 创建一个测试用的时候的store必须在创建的时候必须用combineReducers包裹一层，否则默认的state无法引导到测试组件的props。
 const store = createStore(Reducer);
 // 创建测试对象的时候，因为react-router 升级到了 4.0且用到了 withRouter，如果不想在测试的时候 要特意去除withRouter，必须在创建时加上 Router.
-const provider = (<Provider store={store}><Router history={history}><Home /></Router></Provider>);
+const provider = (<Provider store={store}><Router history={history}><Entry /></Router></Provider>);
 
 // ReactTestUtils example
 // const app = ReactTestUtils.renderIntoDocument(provider);
 // const divisions = ReactTestUtils.scryRenderedDOMComponentsWithTag(app, 'div');
 
 // enzyme example
-const wrapper = mount(provider); // length = 1
+const wrapper = mount(provider);
 // 这个测试用的是react官方的测试组件
-describe('render into document with store and use react test util', () => {
-  it('try to render into document by React（尝试在文本中渲染react）', () => {
-    // react 自带测试套件
-    const app = ReactTestUtils.renderIntoDocument(provider);
-    const divisions = ReactTestUtils.scryRenderedDOMComponentsWithTag(app, 'div');
-    expect(divisions.length).to.equal(3);
-    wrapper.unmount();
-  });
-});
-
-// // 这个测试用的是enzyme的测试组件
-// describe('render into document with store and use enzyme', () => {
-// 	it('try to render into document by enzyme', () => {
-// 		// enzyme 测试组件
-// 		// const wrapper = mount(provider);
-// 		expect(wrapper.find('.App')).to.have.length(1);
-// 	});
+// describe('render into document with store and use react test util', () => {
+//   it('try to render into document by React（尝试在文本中渲染react）', () => {
+//     // react 自带测试套件
+//     const app = ReactTestUtils.renderIntoDocument(provider);
+//     const divisions = ReactTestUtils.scryRenderedDOMComponentsWithTag(app, 'div');
+//     expect(divisions.length).to.equal(3);
+//     wrapper.unmount();
+//   });
 // });
+
+// 这个测试用的是enzyme的测试组件
+describe('render into document with store and use enzyme', () => {
+	it('try to render into document by enzyme', () => {
+		// enzyme 测试组件
+		// const wrapper = mount(provider);
+		wrapper.update();
+		console.log(wrapper.find('.menutype'));
+    wrapper.find({id:'Record'}).first().simulate('click');
+		expect(wrapper.find('div.entry-body')).to.have.length(1);
+	});
+});
